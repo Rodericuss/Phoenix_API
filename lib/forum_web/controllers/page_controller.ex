88d8063c126c8173303203de02a5252cc5a1fd99
Rodericuss <portfoliogithub@outlook.com>
2025-08-18 14:53:52 -0300

    att

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -4,4 +4,16 @@ defmodule ForumWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  def users(conn, _params) do
+    IO.puts("User function hit!")
+
+    users = [
+      %{name: "Cheila", id: 1, email: "rodrigo@gmail.com"},
+      %{name: "Rodrigo", id: 2, email: "evdd@gmail.com"}
+    ]
+
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
